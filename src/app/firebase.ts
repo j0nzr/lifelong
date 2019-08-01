7097d4537554c4795c2f7303c4ad6bfab41df255
Jonas Zeiser <zeiser@zeiser-it.de>
2019-08-01 06:14:00 +0200

    adding required pages, plugins and firebase

---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBpi0ksFPuBvPExfdAUjlGmay81hM7VEZg",
+    authDomain: "lifelong-59c55.firebaseapp.com",
+    databaseURL: "https://lifelong-59c55.firebaseio.com",
+    projectId: "lifelong-59c55",
+    storageBucket: "",
+    messagingSenderId: "965826356748",
+    appId: "1:965826356748:web:cdc51d90bb6642c7"
+  };
+
+  export default firebaseConfig;
