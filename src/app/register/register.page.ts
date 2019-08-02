10a157bcd7e79e800f964419712f486f38439062
Jonas Zeiser <zeiser@zeiser-it.de>
2019-08-02 12:31:12 +0200

    automated changes

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { auth, database } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  public prename: string;
+  public name: string;
+  public email: string;
+  public password: string;
+  public gender: string;
+  public birth: string;
+
+  constructor(public nav: Router, private _location: Location, private afAuth: AngularFireAuth, private afData: AngularFireDatabase) { }
 
   ngOnInit() {
+  }
+
+  backButton(){
+    this._location.back();
+  }
+
+  registerUser(){
+      this.afAuth.auth.createUserWithEmailAndPassword(this.email, this.password);
+      var userId = this.afAuth.auth.currentUser.uid;
+      var userData = this.afData.database.ref('users/'+userId);
+      console.log(userData);
   }
 
 }
