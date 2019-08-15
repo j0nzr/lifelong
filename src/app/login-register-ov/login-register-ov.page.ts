761cfeedbb83d426708ca80efd62b012311b63d2
Jonas Zeiser <zeiser@zeiser-it.de>
2019-08-15 05:43:52 +0200

    get data from facebook/google

--- src/app/login-register-ov/login-register-ov.page.ts
+++ src/app/login-register-ov/login-register-ov.page.ts
@@ -2,9 +2,11 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgxAuthFirebaseUIModule, AuthProvider, Theme } from 'ngx-auth-firebaseui';
 import { auth } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { RegisterPage } from '../register/register.page';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AndroidFullScreen } from '@ionic-native/android-full-screen/ngx';
+import { FacebookAuthProvider } from '@firebase/auth-types';
 
 
 
@@ -15,7 +17,8 @@ import { AndroidFullScreen } from '@ionic-native/android-full-screen/ngx';
 })
 export class LoginRegisterOvPage implements OnInit {
 
-  constructor(public afAuth: AngularFireAuth, public router: Router, private androidFullScreen: AndroidFullScreen){
+  constructor(public afAuth: AngularFireAuth, public router: Router, private androidFullScreen: AndroidFullScreen,
+    private _afData: AngularFirestore, private _alert: AlertController){
     this.androidFullScreen.isImmersiveModeSupported()
       .then(() => console.log('Immersive mode supported'))
       .catch(err => console.log(err));
@@ -28,11 +31,51 @@ export class LoginRegisterOvPage implements OnInit {
   
 
   facebookAuth(){
-    this.afAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
+
+    let provider = new auth.FacebookAuthProvider();
+    provider.addScope("user_birthday, email, user_gender");
+
+    this.afAuth.auth.signInWithPopup(provider).then(
+      (result) => {
+        console.log(result);
+        console.log(this.hasPartner(this.afAuth.auth.currentUser));
+
+        if(this.hasPartner(this.afAuth.auth.currentUser)){
+          this.router.navigateByUrl('/home');
+        }else{
+          var userId = this.afAuth.auth.currentUser.uid;
+          var userData = this._afData.collection('user').doc(userId).set({
+            email: result.additionalUserInfo.profile["email"],
+        gender: result.additionalUserInfo.profile["user_gender"],
+        name: result.additionalUserInfo.profile["last_name"],
+        prename: result.additionalUserInfo.profile["first_name"]
+      }).then(function() {
+        this.router.navigateByUrl('/add-partner');
+      });
+        }
+      },
+      (err)=>{
+        const alert = this._alert.create({
+          header: 'Login Fehler',
+          message: err,
+          buttons: ['Okay']
+        });
+        alert.then((_alert: any)=>{
+          _alert.present();
+        });
+      }
+    );
   }
 
   googleAuth(){
-      this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    let provider = new auth.GoogleAuthProvider();
+
+    provider.addScope("email");
+      this.afAuth.auth.signInWithPopup(provider).then(
+        (result) => {
+          console.log(result);
+        }
+      );
   }
 
   register(){
@@ -41,6 +84,17 @@ export class LoginRegisterOvPage implements OnInit {
 
   login(){
     this.router.navigateByUrl('/login-email');
+  }
+
+  hasPartner(user){
+    let partner = false;
+    /*let data = await this._afData.collection('user').doc(user.uid).get();
+    data.subscribe(x => {
+      if(x.data().keys().find('partner')){
+        partner = true
+      }
+    }); */
+    return partner
   }
 
 }
