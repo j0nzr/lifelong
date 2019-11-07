ab1b11deab386fc8c598f536afe763fe4629fcc7
Jonas Zeiser <zeiser@zeiser-it.de>
2019-11-07 19:07:05 +0100

    Added the 3 Widgets

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Chart } from 'chart.js';
+import { ResultsService } from '../results.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('barChart', {static: false}) barChart;
+  
+  uid: string = this._afAuth.auth.currentUser.uid;
+  partneruid: any;
+  communication: number;
+  honestyTrust: number;
+  needs: number;
+  personality: number;
+  physical: number;
+  time: number;
+  valueDreams: number;
+  best: any[]
+  worst: any[];
 
   constructor(private _router: Router, private _afData: AngularFirestore, private _afAuth: AngularFireAuth) {
     this.hasPartner(this._afAuth.auth.currentUser);
     this.testDone(this._afAuth.auth.currentUser);
+    this._afData.collection('user').doc(this.uid).get().subscribe(async result => {
+      let data = await result.data();
+      this.communication = await data.communication;
+      this.honestyTrust = await data.honestyTrust;
+      this.personality = await data.personality;
+      this.physical = await data.physical;
+      this.valueDreams = await data.valueDreams;
+      this.time = await data.time;
+      this.needs = await data.needs;
+      await this.setBestAndWorst();
+      //this._router.navigateByUrl('/test');
+
+      let results = new ResultsService(_afAuth, _afData, this.barChart);
+      results.start();
+    });
   }
 
   ionViewDidLoad(){
     this.hasPartner(this._afAuth.auth.currentUser);
     this.testDone(this._afAuth.auth.currentUser);
   }
+
+  
 
   public test: any;
   public partner: boolean;
@@ -31,20 +64,29 @@ export class HomePage {
     }
   }
 
+  settings(){
+    this._router.navigateByUrl('/settings');
+  }
+
+  toTest(){
+    this._router.navigateByUrl('/test');
+  }
+
   hasPartner(user){
     let partner = false;
     let data = this._afData.collection('user').doc(user.uid).get().subscribe(
       (result) => {
         if(result.data().partner != ""){
           partner = true;
+          this.partneruid = result.data().partner;
         }else{
           partner = false;
         }
         this.partner = partner;
       }
     );
-    
   }
+
 
   async testDone(user){
     let test = false;
@@ -54,15 +96,31 @@ export class HomePage {
         this.test = test;
       }
     );
+  }
+
+  async setBestAndWorst(){
+    let bestValue = ["", 0];
+    let worstValue = ["", 1000];
+    let arr = [["Kommunikation", this.communication], 
+              ["Ehrlichkeit/Vertrauen", this.honestyTrust],
+              ["Persönlichkeit", this.personality], 
+              ["Physisches", this.physical],
+              ["Werte/Träume", this.valueDreams], 
+              ["Zeit", this.time],
+              ["Bedürfnisse", this.needs]];
+
+    for (let bq of arr){
+      if(bq[1] > bestValue[1]){
+        bestValue = bq;
+      }
+      if(bq[1] < worstValue[1]){
+        worstValue = bq;
+      }
+    }
+
+    this.best = bestValue;
+    this.worst = worstValue;
     
-  }
-
-  settings(){
-    this._router.navigateByUrl('/settings');
-  }
-
-  toTest(){
-    this._router.navigateByUrl('/test');
   }
 
 }
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageModule } from '../home/home.module';
+import { SettingsPageModule } from '../settings/settings.module';
+import { ResultsPageModule } from '../results/results.module';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,7 +18,14 @@ const routes: Routes = [
         { path: 'home', loadChildren: '../home/home.module#HomePageModule' },
         { path: 'settings', loadChildren: '../settings/settings.module#SettingsPageModule' },
         { path: 'results', loadChildren: '../results/results.module#ResultsPageModule'}
-    ]
+    ] 
+    /*path: 'tabs',
+    component: TabsPage,
+    children:[
+    { path: 'home', component: HomePageModule, outlet: 'home'},
+    { path: 'settings', component: SettingsPageModule, outlet: 'settings'},
+    { path: 'results', component: ResultsPageModule, outlet: 'results'},
+    ] */
   },
   {
     path:'',
